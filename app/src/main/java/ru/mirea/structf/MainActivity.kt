af6a625aa65d80a2b8e079b622179acb308c24ed
bluebasooo <blyayachapoveshuci@yandex.ru>
2023-05-07 20:10:03 +0300

    main activity + test for fragments

--- app/src/main/java/ru/mirea/structf/MainActivity.kt
+++ app/src/main/java/ru/mirea/structf/MainActivity.kt
@@ -2,10 +2,20 @@ package ru.mirea.structf
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import ru.mirea.structf.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        getSupportActionBar()?.setDisplayShowTitleEnabled(false)
+        setContentView(mainBinding.root)
+        mainBinding.homeButton.setOnClickListener {
+            Toast.makeText(applicationContext, "This home button", Toast.LENGTH_SHORT).show()
+        }
     }
 }
